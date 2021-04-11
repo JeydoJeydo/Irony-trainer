16ca204a060850ab863e57cd1e32d3c49e12f839
Silas <silas@sisyfox.com>
2021-04-11 16:27:05 +0200

    added intro text

--- lvlData.js
+++ lvlData.js
@@ -16,51 +16,67 @@ var lvlData = [
     "userAnswer":""
 },
 {
-    "type":1,
-    "text":"dritter Test bla bla",
+    "type":0,
+    "text":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "task":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "ifWrong":"secondse",
     "solution":0,
     "userAnswer":""
 },
 {
     "type":0,
-    "text":"hallo der blaaaaa",
-    "solution":2,
-    "userAnswer": ""
+    "text":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "task":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "ifWrong":"secondse",
+    "solution":0,
+    "userAnswer":""
 },
 {
     "type":0,
-    "text":"hallo der blaaaaa",
-    "solution":2,
-    "userAnswer": ""
+    "text":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "task":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "ifWrong":"secondse",
+    "solution":0,
+    "userAnswer":""
 },
 {
     "type":0,
-    "text":"hallo der blaaaaa",
-    "solution":2,
-    "userAnswer": ""
+    "text":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "task":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "ifWrong":"secondse",
+    "solution":0,
+    "userAnswer":""
 },
 {
     "type":0,
-    "text":"hallo der blaaaaa",
-    "solution":2,
-    "userAnswer": ""
+    "text":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "task":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "ifWrong":"secondse",
+    "solution":0,
+    "userAnswer":""
 },
 {
     "type":0,
-    "text":"hallo der blaaaaa",
-    "solution":2,
-    "userAnswer": ""
+    "text":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "task":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "ifWrong":"secondse",
+    "solution":0,
+    "userAnswer":""
 },
 {
     "type":0,
-    "text":"hallo der blaaaaa",
-    "solution":2,
-    "userAnswer": ""
+    "text":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "task":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "ifWrong":"secondse",
+    "solution":0,
+    "userAnswer":""
 },
 {
     "type":0,
-    "text":"hallo der blaaaaa",
-    "solution":2,
-    "userAnswer": ""
+    "text":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "task":"Hermann stieg zusammen mit seinen Freunden die Treppe hinunter als er plötzlich den Halt verlohr und mitsammt seinen Sachen unter einem riesen Knall die Treppe hinunten viel, unten angekommen stand der rasch wieder auf und sagte Da wäre ich ja fast hingefallen",
+    "ifWrong":"secondse",
+    "solution":0,
+    "userAnswer":""
 }
 ]
