a1ab0b6a77e1a0a7bec2395f72e6681299de41c2
Silas <silas@sisyfox.com>
2021-05-09 13:27:00 +0200

    added new lvls

--- lvlData.js
+++ lvlData.js
@@ -1,24 +1,25 @@
 //<img src=\"img/lock.png\" alt=\"\" width=\"100%\">     img tag
+// ironie, lüge, fehler, keine Ironie  0, 1, 2, 3
 var lvlData = [
 {
     "type":2,
-    "text":"Ironie beginnt sich ab fünf Jahren bis über 13 Jahre hinaus zu entwickeln. Ironie kann oft mit einer Lüge oder einem Fehler verwechselt werden. Um Ironie zu erkennen, durchläuft man drei Schritte. Im ersten Schritt muss erkannt werden, dass die Aussage nicht so gemeint ist, wie sie gesagt wurde. Im Zweiten muss die Beziehung zwischen Gesagtem und Gemeintem erkannt werden. Da die Aussage meist auf die Situation bezogen ist, fällt dieser Schritt den meisten Kindern am leichtesten. Im letzten Schritt muss erkannt werden, was der Sprecher wirklich gemeint hat.<br><br><i>Jetzt bist du dran.</i>",
-    "task":"Ein farbenblinder Mann betritt die Bar, zeigt auf die rote Tapete und sagt:,,die ist aber schön grün\".<br><br><i>Ist diese Aussage eine ironische, gelogene oder fehlerhafte?</i>",
-    "ifWrong":"Leider falsch, da der Mann farbenblind ist, kann er nicht wissen, welche Farbe die Tapete hat, deswegen ist die Aussage fehlerhaft.",
-    "solution":4,
-    "userAnswer":""
-},
-{
-    "type":1,
-    "text":"Am bekanntesten ist die Ironie auf der Wortebene, die Ironie, die wir aussprechen. Herkömmlich ist sie als Spott oder Gegenteil zu gesagtem bekannt.<br><br><i>Die Schritte um sie zu verstehen kennst du ja schon</i>",
-    "task":"Wenn sich jemand mit Absicht dummstellt:,,Das habe ich ja gar nicht gewusst\"",
-    "ifWrong":"noch kein Kontent",
+    "text":"Ironie beginnt sich ab fünf Jahren zu entwickeln.<br>Sie zählt zu den wichtigsten sprachlichen Fähigkeiten, die du erlernen kannst.<br>Zum glück ist Ironie etwas, was du einfach nebenbei lernen willst.<br>Diese App soll einfach eine kleine Unterstützung bieten und dir dabei helfen noch viel schneller zu lernen als du es ohnehin schon getan hättest.<br><br>Um Ironie zu verstehen durch läufst du unterbewusst mehrere Schritte, von denen du meist gar nichts mitbekommst.<br><br>Im ersten Schritt musst du erkennen, dass die Aussage nicht so gemeint ist, wie sie gesagt wurde.<br>Im zweiten Schritt musst du erkennen, was die Aussage überhaupt mit der Situation zu tun hat.<br>Und im letzten und dritten Schritt musst du heraus bekommen, was wirklich gemeint ist.<br>Klingt einfach und das ist es eigentlich auch.<br><br>Grundlegend kannst du dir merken, dass du dann von Ironie sprechen kannst, wenn die Erscheinung einer Situation nicht mit der Wahrheit übereinstimmt, also das Gegenteil vom eigentlich gemeinten.",
+    "task":"Jetzt weißt du also, dass du von Ironie sprechen kannst, wenn die Erscheinung nicht mit der Wahrheit übereinstimmt.<br><br>Stell dir also ein Schild vor, auf dem steht, dass der Besitzers kein Geld für ein echtes Schild hatte, ist das Ironie?",
+    "ifWrong":"Leider falsch, die Erscheinung ist, dass auf dem Schild steht, dass er Besitzer des Schildes kein Geld für ein Schild hat, jedoch trägt er ein Schild. Erscheinung und Wahrheit stimmen also nicht überein.",
     "solution":0,
     "userAnswer":""
 },
 {
     "type":1,
-    "text":"Doch es gibt auch weitere Formen, in denen nicht nur das bloße Gegenteil ausgedrückt wird. Man kann beispielsweise etwas kritisieren und gleichzeitig der Situation den Ernst nehmen.<br><br><i>Wichtig ist nur, dass sicher gestellt wird, dass das Gegenüber die Ironie versteht. Sonst kann es die Situation noch verschlimmern.</i>",
+    "text":"Insgesamt gibt es drei verschiedene Arten von Ironie. Am bekanntesten ist die Ironie auf der Wortebene, auch als einfaches Gegenteil zum gesagten bekannt.<br>Im ersten Level hast du schon die Gegenteils Ironie kennengelernt.<br>Doch lass es uns etwas schwerer machen. Ironie kann oft mit einer Lüge oder einem Fehler verwechselt werden. Deswegen sind die beschriebenen drei Schritte so wichtig. Kannst du sie schon unterscheiden?",
+    "task":"Stell dir vor, du lernst gerade plusrechnen und liest in deinem Mathebuch eine Erklärung in der eine Aufgabe vorgerechnet ist, damit du verstehst wie Plusrechnung funktioniert.<br><br>Die Aufgabe lautet:  5 + 10  = 20<br><br><i>Überprüfe, ob diese Aussage ironisch, gelogen oder fehlerhaft ist.</i>",
+    "ifWrong":"Die Erklärung einer Aufgabe dient in einem Mathebuch dazu den Schüler praktisch zu zeigen, wie man eine Aufgabe rechnet, du musst nicht von Ironie oder einer Lüge ausgehen, deswegen muss es ein Fehler sein.",
+    "solution":2,
+    "userAnswer":""
+},
+{
+    "type":1,
+    "text":"Eine weitere Art sind ironische Untertreibungen. Diese kennst du bestimmt als ironische Kritik. Das \,,hast du ja toll gemacht\”, wenn dir etwas herunter gefallen ist, hast du bestimmt schon gehört. Genau das ist eine ironische Kritik.<br><br><i>Falls es dich interessiert, ironische Kritik wird schon von fünf bis Siebenjährigen oft richtig erkannt.</i>",
     "task":"Wenn sich jemand wirklich gar keine Mühe bei einer Aufgabe gegeben hat und der Lehrer sagt:,,das hast du aber toll gemacht\"",
     "ifWrong":"noch kein Kontent",
     "solution":0,
