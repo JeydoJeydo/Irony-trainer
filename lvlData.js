bdb09736de419a7c0a2c7c4a158f56f3a019b3fe
Silas <silas@sisyfox.com>
2021-05-09 14:06:06 +0200

    added more lvls

--- lvlData.js
+++ lvlData.js
@@ -1,4 +1,4 @@
-//<img src=\"img/lock.png\" alt=\"\" width=\"100%\">     img tag
+//<img src=\"img/lvl/placeholder.png\" alt=\"image\" width=\"100%\">     img tag
 // ironie, lüge, fehler, keine Ironie  0, 1, 2, 3
 var lvlData = [
 {
@@ -20,22 +20,22 @@ var lvlData = [
 {
     "type":1,
     "text":"Eine weitere Art sind ironische Untertreibungen. Diese kennst du bestimmt als ironische Kritik. Das \,,hast du ja toll gemacht\”, wenn dir etwas herunter gefallen ist, hast du bestimmt schon gehört. Genau das ist eine ironische Kritik.<br><br><i>Falls es dich interessiert, ironische Kritik wird schon von fünf bis Siebenjährigen oft richtig erkannt.</i>",
-    "task":"Wenn sich jemand wirklich gar keine Mühe bei einer Aufgabe gegeben hat und der Lehrer sagt:,,das hast du aber toll gemacht\"",
+    "task":"Wir können ja direkt das Beispiel nehmen Während du dir einen Joghurt aus dem Kühlschrank nimmst fällt dir ein anderer heraus und verteilt sich auf dem ganzen Boden.<br><br></br>Deine Mutter hinter dir hat alles gesehen und sagt, \,,das hast du aber toll gemacht\” Ist diese Reaktion Ironie, eine Lüge oder ein Fehler?",
     "ifWrong":"noch kein Kontent",
     "solution":0,
     "userAnswer":""
 },
 {
     "type":1,
-    "text":"noch kein Kontent",
-    "task":"noch kein Kontent",
+    "text":"Die letzte fehlende Form ist die Sitationsironie, sie tritt dann auf, wenn Erwartungen verletzt oder ungültig gemacht werden.",
+    "task":"Stell dir vor, dass du dich den ganzen Tag auf einen Besuch bei deinem besten Freund oder Freundin gefreut hast, doch kurz bevor ihr losfahren könnt fängt es an zu regnen und ihr könnt nicht fahren.<br><br>Wo würdest du dieses Ereignis zuordnen?",
     "ifWrong":"noch kein Kontent",
     "solution":0,
     "userAnswer":""
 },
 {
     "type":1,
-    "text":"noch kein Kontent",
+    "text":"Jetzt wo du ungefähr weißt wovon du redest und dich mit Ironie auskennst, kannst du mir bei etwas helfen? Dort hinter den Bergen gibt es etwas, was ich gerne hätte für … ein Experiment von dem ich dir jetzt noch nicht erzählen kann.<br><br><img src=\"img/lvl/placeholder.png\" alt=\"image\" width=\"100%\"><br><br>Doch der Weg wird kein Spaziergang, dann hätte ich es einfach selber gemacht.<br>Wenn du dich bereit fühlst dann mache dich auf den Weg, ich warte hier auf dich.",
     "task":"noch kein Kontent",
     "ifWrong":"noch kein Kontent",
     "solution":0,
